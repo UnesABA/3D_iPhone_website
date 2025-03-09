00e3d42a1b5f76d03834ef995e41727ef2490f7c
UnesABA <unesaitbenali@gmail.com>
2025-03-09 13:46:44 +0100

    Add Highlights section with animated title and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <main className= "bg-black">
         <Navbar />
         <Hero />
+        <Highlights />
       </main>
     </>
   )
