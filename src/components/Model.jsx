3bf65d1bf68bb933b1ab86a4728c9cf87c409824
UnesABA <unesaitbenali@gmail.com>
2025-03-12 14:24:44 +0100

    Optimize model component layout and internal code structure for improved performance

---
+++ src/components/Model.jsx
@@ -0,0 +1,118 @@
+import { useGSAP }          from '@gsap/react'
+import gsap                 from "gsap"
+import ModelView            from './ModelView'
+import { useRef, useState } from 'react'
+import {yellowImg}          from "../utils"
+import * as THREE           from "three"
+import {View}               from "@react-three/drei"
+import {models, sizes}      from "../constants/index"
+
+const Model = () => {
+  const [size, setSize]   = useState("small")
+  const [model, setModel] = useState({
+    title: "iphone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "6F6C64"],
+    img  : yellowImg, 
+  })
+
+  // camera control for the model view 
+  const cameraControlSmall = useRef()
+  const cameraControlLarge = useRef()
+
+  //keep track of the MODEL to access its properties when animating  
+  const small = useRef(new THREE.Group())
+  const large = useRef(new THREE.Group())
+
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0)
+  const [largeRotation, setLargeRotation] = useState(0)
+
+  useGSAP(() =>{
+    gsap.to("#heading", {
+      y      : 0,
+      opacity: 1
+    })
+  }, [])
+
+  return (
+    <section className= "common-padding">
+      <div className= "screen-max-width">
+        <h1 id= "heading" className= "section-heading">
+          Take a closer look
+        </h1>
+
+        <div className= "flex flex-col items-center mt-5">
+          <div className= "w-full h-[75vh] md:[90vh] overflow-hidden relative">
+            <ModelView 
+              index           = {1}
+              groupRef        = {small}
+              gsapType        = "View1"
+              controlRef      = {cameraControlSmall}
+              setRotationState= {setSmallRotation}
+              item            = {model}
+              size            = {size}
+            />
+            <ModelView 
+              index           = {2}
+              groupRef        = {large}
+              gsapType        = "View2"
+              controlRef      = {cameraControlLarge}
+              setRotationState= {setLargeRotation}
+              item            = {model}
+              size            = {size}
+            />
+
+            <canvas
+              className= "w-full h-full"
+              style= {{
+                position: "fixed",
+                top     : 0,
+                right   : 0,
+                bottom  : 0,
+                left    : 0,
+                overflow: "hidden"
+              }} 
+              // when we want to interact with the model 
+              eventSource= {document.getElementById("root")} 
+            >
+              <View.Port />
+            </canvas>
+          </div>
+          <div className= "mx-auto w-full">
+            <p className= "text-sm font-light text-center mb-5">{model.title}</p>
+
+            <div className= "flex-center">
+              <ul className= "color-container">
+                {models.map((item, i) =>(
+                  <li 
+                  key      = {i} 
+                  className= "w-6 h-6 rounded-full mx-2 cursor-pointer"
+                  style    = {{backgroundColor: item.color[0]}}
+                  onClick  = {() => setModel(item)}
+                  />
+                ))}
+              </ul>
+
+              <div className= "size-btn-container">
+                {sizes.map(({value, label}) =>(
+                  <span 
+                    key= {label}
+                    className= "size-btn cursor-pointer"
+                    style    = {{backgroundColor: size === value ? "white" : "transparent",
+                                 color          : size === value ? "black" : "white"  
+                                }}
+                    onClick  = {() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Model
