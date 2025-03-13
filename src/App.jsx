b25a464e9fc39005ebc84e12190f1d077904bd89
UnesABA <unesaitbenali@gmail.com>
2025-03-14 00:20:16 +0100

    Integrate Sentry for error tracking and configure GSAP plugins for animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import Navbar     from "./components/Navbar"
-import Hero       from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model      from "./components/Model"
+import Navbar      from "./components/Navbar"
+import Hero        from "./components/Hero"
+import Highlights  from "./components/Highlights"
+import Model       from "./components/Model"
+import * as Sentry from "@sentry/react"
 
 function App() {
   return (
-    <>
-      <main className= "bg-black">
-        <Navbar />
-        <Hero />
-        <Highlights />
-        <Model />
-      </main>
-    </>
+    <main className= "bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import  React , { StrictMode} from 'react'
+import * as Sentry            from "@sentry/react"
+import { createRoot }         from 'react-dom/client'
+import App                    from './App.jsx'
 import './index.css'
-import App            from './App.jsx'
+
+Sentry.init({
+  dsn: "https://your-correct-dsn@sentry.io/your-project-id",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
