e19be99e557f3a99bd34fc694e473c693ca3cfb9
UnesABA <unesaitbenali@gmail.com>
2025-03-15 01:44:04 +0100

    Create Features section with GSAP animations and interactive video

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar      from "./components/Navbar"
 import Hero        from "./components/Hero"
 import Highlights  from "./components/Highlights"
 import Model       from "./components/Model"
+import Features    from "./components/Features"
 import * as Sentry from "@sentry/react"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
